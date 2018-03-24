76a30db64cfddb8c886c9f4377e7e8c61125c735
nathan07 <nathan.prinsac@iut-valence.fr>
2018-03-24 16:21:08 +0100

    debut recuperation des lignes

--- app/src/main/java/com/grenoble/miage/metrobilite/DataCollector.java
+++ app/src/main/java/com/grenoble/miage/metrobilite/DataCollector.java
@@ -1,14 +1,19 @@
 package com.grenoble.miage.metrobilite;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Created by U Mag on 21/03/2018.
+ * Created by prinsacn on 21/03/2018.
  */
 
 public class DataCollector {
@@ -30,5 +35,37 @@ public class DataCollector {
 
         System.out.println("response : "+conn.getResponseCode());
 
+        InputStream inputStream = new BufferedInputStream(conn.getInputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder result = new StringBuilder();
+
+        String line;
+
+        while((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+
+        try {
+            JSONArray json = new JSONArray(result.toString());
+            System.out.println("json: "+json);
+            JSONObject[] lignes = new JSONObject[json.length()];
+            int indexLignes = 0;
+            for(int i = 0;i<json.length();i++) {
+                if(json.getJSONObject(i).getString("id").contains("SEM")) {
+                    lignes[indexLignes] = json.getJSONObject(i);
+                    indexLignes++;
+                }
+
+            }
+
+            for(int i = 0;i<indexLignes;i++) {
+                System.out.println("lignes["+indexLignes+"] : "+ lignes[i]);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
     }
+
 }
